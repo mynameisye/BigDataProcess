155be49667da41bb9f78c8d0cdc4443cf6988eb9
mynameisye <102737288+mynameisye@users.noreply.github.com>
2023-11-10 11:54:31 +0900

    UBERStudent20201004.py

--- HW2/UBERStudent20201004.py
+++ HW2/UBERStudent20201004.py
@@ -6,7 +6,7 @@ dayofweek = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
 
 f = open(sys.argv[1])
 f2 = open(sys.argv[2], "wt")
-sen = []
+sen_dict = dict()
 
 for line in f:
     line = line.strip()
@@ -14,10 +14,18 @@ for line in f:
 
     date = uber[1].split("/")
     day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
-    sen.append("%s,%s %s,%s\n" % (uber[0],dayofweek[day],uber[2],uber[3]))
 
-for s in sen:
-    f2.write(s)
+    sId = uber[0] + "," + dayofweek[day]
+    if sId in sen_dict:
+        val = sen_dict[sId].split(",")
+        va11 = int(uber[2]) + int(val[0])
+        val2 = int(uber[3]) + int(val[1])
+    else:
+        sen_dict[sId] =  uber[2] + "," + uber[3]
+
+
+for s in sen_dict.keys():
+    f2.write(s + " " + sen_dict[s] + "\n")
 
 f.close()
 f2.close()
